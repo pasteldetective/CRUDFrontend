d7a90dab3d2c66631b86fa37482738633955e9b3
Jackie Yee <pi.jackyy@gmail.com>
2024-05-20 22:10:09 -0400

    updated readme

--- database/utils/configDB.js
+++ database/utils/configDB.js
@@ -6,7 +6,7 @@ It declares and exports the variables for database name, username, and password.
 // Declare the variables for database name, username, and password.
 const dbName = 'starter-server';
 const dbUser = 'postgres';
-const dbPwd = '123';  // Note: If needed, change this password to match the password created for PostgreSQL database on the local machine.
+const dbPwd = 'root';  // Note: If needed, change this password to match the password created for PostgreSQL database on the local machine.
 
 // Export the variables 
 module.exports = {
